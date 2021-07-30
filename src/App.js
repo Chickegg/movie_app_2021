1323e65b0d1b51f6a711d0e0b40392ab78c08677
gkswn45 <73797608+gkswn45@users.noreply.github.com>
2021-07-30 23:58:46 +0900

    v.0.02
    JSX + Props로 재사용 가능한 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Potato from "./Potato";
+import React from "react";
+
+function Food({fav}) {
+  return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
-    <div>
-      <div className="App">hello!!!!!</div>
-      <Potato />
-    </div>
+      <div className="App">
+        <h1>hello!!!!!</h1>
+        <Food fav="kimchi" />
+        <Food fav="ramen"/>
+        <Food fav="samgiopsal"/>
+        <Food fav="chukumi"/>
+
+        </div>
   );
 }
 
