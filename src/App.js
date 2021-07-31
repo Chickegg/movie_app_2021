2c3bd0ebf90ebf067d9881f755177150aa9d0650
gkswn45 <73797608+gkswn45@users.noreply.github.com>
2021-07-31 23:58:58 +0900

    prop-types 확인기능 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,74 @@
 import React from "react";
+import PropTypes from "prop-types"
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+      rating: 5
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+      rating: 4.9
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+      rating: 4.8
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+      rating:4.7
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+      rating: 4.6
+  }
+];
+
+function Food({name, picture, rating}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name}/>
+    </div>
 }
 
+Food.propTypes = {
+  name: PropTypes.string,
+  picture: PropTypes.string,
+  rating: PropTypes.number
+}
+// propTypes라고만 지어야한다.
+
+
+function renderFood(dish) {
+  console.log();
+  return <Food name={dish.name} picture={dish.image} />
+}
 function App() {
   return (
       <div className="App">
-        <h1>hello!!!!!</h1>
-        <Food fav="kimchi" />
-        <Food fav="ramen"/>
-        <Food fav="samgiopsal"/>
-        <Food fav="chukumi"/>
-
-        </div>
+        {foodILike.map(dish => (
+        <Food key = {dish.id} 
+        name ={dish.name} 
+        picture = {dish.image} 
+        rating = {dish.rating} />
+        ))}
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // 같은 directory안에 있는 App이라는 함수를 불러온다.
-import Potato from './App';
 
 ReactDOM.render(
   <React.StrictMode>
