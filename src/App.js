84d3160282ac8fa9dab7c1ae48b55be9af250097
gkswn45 <73797608+gkswn45@users.noreply.github.com>
2021-08-01 22:45:01 +0900

    class와 state를 통한 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,75 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types"
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-      rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-      rating: 4.9
 
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-      rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-      rating:4.7
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating: 4.6
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   }
-];
-
-function Food({name, picture, rating}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name}/>
-    </div>
-}
-
-Food.propTypes = {
-  name: PropTypes.string,
-  picture: PropTypes.string,
-  rating: PropTypes.number
-}
-// propTypes라고만 지어야한다.
-
-
-function renderFood(dish) {
-  console.log();
-  return <Food name={dish.name} picture={dish.image} />
-}
-function App() {
-  return (
-      <div className="App">
-        {foodILike.map(dish => (
-        <Food key = {dish.id} 
-        name ={dish.name} 
-        picture = {dish.image} 
-        rating = {dish.rating} />
-        ))}
-      </div>
-  );
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading : false});
+    }, 6000);  
+  }
+      // delay function 6초 뒤에 실행될 것이다.
+      // 6초뒤에 state의 isLoading은 false로 바뀐다 그때 생기는 변화는?
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading": "We are ready"}</div> 
+  }
+  // 삼항연산자 true라면 "Loading" false라면 We are ready를 screen에 render한다.
+  // 즉 delay function 이 있기 때문에 6초동안은 true이기때문에 Loading이
+  // 6초뒤에는 false로 바뀌기 때문에 We are ready로 바뀔 것이다.
 }
 
 export default App; // 앱이라는 함수를 내보낸다.
