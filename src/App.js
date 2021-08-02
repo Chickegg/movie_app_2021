30bfa825a63aabfd66ee30b943d188ceaebb6336
unknown <73797608+Chickegg@users.noreply.github.com>
2021-08-02 23:56:45 +0900

    API를 통해 Movies의 정보 받아오기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
-  }
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading : false }); // {movies:movies}라고 해야하난 ES6에서는 생랼해서 { movies }가 가능하다.
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 6000);  
+    this.getMovies();
   }
-      // delay function 6초 뒤에 실행될 것이다.
-      // 6초뒤에 state의 isLoading은 false로 바뀐다 그때 생기는 변화는?
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading": "We are ready"}</div> 
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
-  // 삼항연산자 true라면 "Loading" false라면 We are ready를 screen에 render한다.
-  // 즉 delay function 이 있기 때문에 6초동안은 true이기때문에 Loading이
-  // 6초뒤에는 false로 바뀌기 때문에 We are ready로 바뀔 것이다.
 }
-
-export default App; // 앱이라는 함수를 내보낸다.
+export default App;
